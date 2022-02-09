44a0ff3e6e60d69c9e5e0ee6cac690c7737c0b6d
William Cuthbertson <whc1315@gmail.com>
2022-02-09 10:58:12 -0700

    Added config for Heroku

--- server/server.js
+++ server/server.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(4000, () => console.log("Server is running on 4000"));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../public/index.html"));
+});
+
+const port = process.env.PORT || 4000;
+
+app.listen(port, () => console.log(`Server is running on ${port}`));
